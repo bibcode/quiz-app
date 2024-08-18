426ec1ec4e63e1dd7356ba8a940dc5e2d65cc79d
Ibrahim Muhammad <bibcodes5@gmail.com>
2024-08-18 15:42:20 +0100

    first commit

---
+++ src/home.js
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+        <div>
+            <h1>A simple quiz app</h1>
+            <button>Start quiz</button>
+        </div>
+    );
+}
+ 
+export default Home;
