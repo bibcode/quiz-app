b5579523267fd3002260f1bf1b8aabc426723e84
Ibrahim Muhammad <bibcodes5@gmail.com>
2024-09-03 17:21:11 +0100

    add new button

--- src/container.js
+++ src/container.js
@@ -2,24 +2,30 @@ import { useState } from "react";
 import questions from "./question";
 
 const Container = () => {
-    const [currentQuestion, setCurrentQuestion]=useState(0);
-    const [showScore, setShowScore]=useState(false);
-    const [score, setScore] =useState(0);
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [showScore, setShowScore] = useState(false);
+    const [score, setScore] = useState(0);
 
-
-    const handleAnswerOptionClick =(iscorrect)=>{
-        if (iscorrect){
-            setScore(score + 1)
+    const handleAnswerOptionClick = (iscorrect) => {
+        if (iscorrect) {
+            setScore(score + 1);
         }
 
         const nextQuestion = currentQuestion + 1;
         if (nextQuestion < questions.length) {
-            setCurrentQuestion(nextQuestion)
+            setCurrentQuestion(nextQuestion);
         } else {
+            setShowScore(true);
+        }
+    };
+
+    const next =()=>{
+        const nextQuestion = currentQuestion + 1;
+        setCurrentQuestion(nextQuestion)
+        if (nextQuestion === 10) {
             setShowScore(true)
         }
     }
-
 
     return (
         <div className="cont">
@@ -27,29 +33,34 @@ const Container = () => {
                 <div className="score-section">
                     You scored {score} out of {questions.length}
                 </div>
-            ) :(
-            
+            ) : (
                 <div className="question-section">
                     <div className="question-count">
-                        <span>Question {currentQuestion +1}/{questions.length}</span>
+                        <span>
+                            Question {currentQuestion + 1}/{questions.length}
+                        </span>
                     </div>
-                    <div className="question-text">{questions[currentQuestion].questionText}
-
+                    <div className="question-text">
+                        {questions[currentQuestion].questionText}
                     </div>
                     <div className="answer-section">
-                    {questions[currentQuestion].answerOptions.map((answerOption, index) => (
-                        <button key={index} onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>
-                            {answerOption.answerText}
+                        {questions[currentQuestion].answerOptions.map((answerOption) => (
+                            <button
+                                onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}
+                            >
+                                {answerOption.answerText}
                             </button>
-                    ))}
+                        ))}
                     </div>
+                    <button className="nextQ"
+                    onClick={()=>next()}
+                    >
+                        NextQuestion
+                    </button>
                 </div>
-                
-            )
-
-            }
+            )}
         </div>
     );
-}
- 
+};
+
 export default Container;
